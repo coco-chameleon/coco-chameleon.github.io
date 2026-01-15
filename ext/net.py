3db96c786b05e22a0770e587aed6d95ea79d7ce2
Sam Hanes <sam@maltera.com>
2026-01-15 02:28:12 -0800

    Add negative overlines to net role.

--- ext/net.py
+++ ext/net.py
@@ -8,7 +8,20 @@ from sphinx.util.typing import ExtensionMetadata
 
 class NetRole(SphinxRole):
     def run(self) -> tuple[list[nodes.Node], list[nodes.system_message]]:
-        node = nodes.literal(text=self.text)
+        node = nodes.literal(classes=["net"])
+
+        stars = self.text.split("*")
+        while len(stars) > 1:
+            slash = stars.pop(0).rsplit('/', 1)
+            if len(slash) > 1:
+                node.append(nodes.inline(text=slash[0] + '/', classes=["net-pos"]))
+                node.append(nodes.inline(text=slash[1], classes=["net-neg"]))
+            else:
+                node.append(nodes.inline(text=slash[0], classes=["net-neg"]))
+
+        if len(stars) > 0 and stars[0] != "":
+            node.append(nodes.inline(text=stars[0], classes=["net-pos"]))
+
         return [node], []
 
 def setup(app: Sphinx) -> ExtensionMetadata:
